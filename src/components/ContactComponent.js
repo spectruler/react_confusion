fe26fe06827d0fe19dd0ae2d1839b59456ff8832
jaipal <jaipalkumar1293@gmail.com>
2020-07-20 19:02:26 +0500

    Uncontrolled forms

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -54,37 +54,31 @@ class Contact extends Component {
             telnum: '',
             email: ''
         };
-
         if (this.state.touched.firstname && firstname.length < 3){
             errors.firstname = 'First Name should be >= 3 characters';
         }
         else if(this.state.touched.firstname && firstname.length >10){
             errors.firstname = 'First Name should be <= 10 characters';
         }
-
         if (this.state.touched.lastname && lastname.length < 3){
             errors.lastname = 'Last Name should be >= 3 characters';
         }
         else if(this.state.touched.lastname && lastname.length >10){
             errors.lastname = 'Last Name should be <= 10 characters';
         }
-
         const reg = /^\d+$/;
-        if(this.state.touched.telnum && !reg.test(telnum)){
+        if(this.state.touched.telnum && !reg.test(telnum)) {
             errors.telnum = 'Tel. Number should contain only numbers';
         }
-
         if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1){
             errors.email = 'Email should contain a @';
         }
-
         return errors;
     }
 
 
     render() {
         const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email);
-
         return (
             <div className="container">
                 <div className="row">
