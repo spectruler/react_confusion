8feb5f861d733e16a76f7e94e1500a2dcb3fa1f3
jaipal <jaipalkumar1293@gmail.com>
2020-07-24 17:46:41 +0500

    week 4 complete

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from "../shared/baseUrl";
+import {Loading} from "./LoadingComponent";
+import { Fade, Stagger } from 'react-animation-components';
 
 
 function RenderLeader({ leader }){
@@ -8,7 +11,7 @@ function RenderLeader({ leader }){
         <div key={leader.id}  className="col-12 mt-5">
             <Media tag='li'>
                 <Media left middle>
-                    <Media object src={leader.image} alt={leader.name}/>
+                    <Media object src={baseUrl + leader.image} alt={leader.name}/>
                 </Media>
                 <Media body className='ml-5'>
                     <Media heading>{leader.name}</Media>
@@ -22,11 +25,16 @@ function RenderLeader({ leader }){
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
-        return (
-            <RenderLeader leader={leader}/>
-        );
-    });
+    if (props.leaderLoading){
+        return <Loading />
+    }else if (props.leaderErrMess){
+        return <h4>{props.leaderErrMess}</h4>
+    }
+    const leaders  = props.leaders.map(leader=>(
+        <Fade in key={leader.id}>
+            return <RenderLeader leader={leader}/>
+        </Fade>
+    ));
 
     return<div className="container">
         <div className="row">
@@ -83,7 +91,9 @@ function About(props) {
             </div>
             <div className="col-12">
                 <Media list>
-                    {leaders}
+                    <Stagger in>
+                        {leaders}
+                    </Stagger>
                 </Media>
             </div>
         </div>
